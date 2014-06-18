159afa571a0d5a4a0dcba843d7e23f5994eef664
disdi <saket.sinha89@gmail.com>
2014-06-18 20:06:40 +0530

    Clocking support for Omap sdhci driver(2)

--- linux-3.15-rc8/drivers/mmc/host/sdhci-omap.h
+++ linux-3.15-rc8/drivers/mmc/host/sdhci-omap.h
@@ -24,4 +24,13 @@
 #define OMAP_CTRL_8BITBUS          (0x2 << 1)
 #define OMAP_CTRL_BUSWIDTH_MASK    (0x3 << 1)
 
+#define OMAP_SYSTEM_CONTROL    0x2c
+#define OMAP_CLOCK_MASK        0x0000fff0
+#define OMAP_PREDIV_SHIFT      8
+#define OMAP_DIVIDER_SHIFT     4
+#define OMAP_CLOCK_PEREN       0x00000004
+#define OMAP_CLOCK_HCKEN       0x00000002
+#define OMAP_CLOCK_IPGEN       0x00000001
+
+
 #endif /* _DRIVERS_MMC_SDHCI_OMAP_H */
